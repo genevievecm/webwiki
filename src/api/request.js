5ea77cb901951d7164a37d385037c4a81eb4dda8
Genevieve Moreau <gmoreau@forgemedia.ca>
2017-03-31 17:30:10 -0400

    using fetch to get WP api response

--- src/api/request.js
+++ src/api/request.js
@@ -1,13 +1,15 @@
 import WP_API_BASE_URL from './config';
 import 'whatwg-fetch';
 
-fetch(WP_API_BASE_URL, {
-	method: 'get'
-}).then(function(response) {
-	return response.json();
-})
-.then(function(parsedData) {
-    console.log(parsedData);
-}).catch(function(err) {
-	console.log(err);
-});
+export function request(endpoint, method = 'GET') {
+	return fetch(WP_API_BASE_URL, {
+		method: 'get'
+	}).then(function(response) {
+		return response.json();
+	})
+	.then(function(parsedData) {
+	    console.log(parsedData);
+	}).catch(function(err) {
+		console.log(err);
+	});
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import { WP_API_BASE_URL } from '../../api/config';
-import 'whatwg-fetch';
 
 export default class App extends Component {
 
 	componentDidMount(){
-		fetch(WP_API_BASE_URL, {
-			method: 'get'
-		}).then(function(response) {
-    		return response.json();
-		})
-		.then(function(parsedData) {
-		    console.log(parsedData);
-		}).catch(function(err) {
-			console.log(err);
-		});
+		
 	}
 
   render() {
